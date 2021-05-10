e58ca30e2f5eb2461e8e8a60bef24a13db229843
endigma <endigma@cya.cx>
2021-05-09 23:17:43 -0300

    Move viper init to main.go, add config option for fancy logs

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -16,8 +17,61 @@ import (
 )
 
 func init() {
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).With().Caller().Logger()
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+
+	viper.AddConfigPath("/etc/holden/")
+	viper.AddConfigPath("$HOME/.holden")
+	viper.AddConfigPath(".")
+
+	viper.SetDefault("general.docroot", "docs")
+	viper.SetDefault("general.port", "11011")
+	viper.SetDefault("general.prefix", "")
+	viper.SetDefault("general.workdir", "")
+	viper.SetDefault("general.allowhtml", false)
+	viper.SetDefault("general.linkablelines", true)
+	viper.SetDefault("general.fancystdout", true)
+	viper.SetDefault("general.fancycurl", true)
+	viper.SetDefault("general.debug", false)
+	viper.SetDefault("website.sitename", "")
+	viper.SetDefault("website.backtotop", true)
+	viper.SetDefault("website.sidebar", true)
+	viper.SetDefault("aesthetic.highlightstyle", "solarized-dark256")
+	viper.SetDefault("aesthetic.linenumbers", true)
+	viper.SetDefault("aesthetic.linenumbersintable", true)
+	viper.SetDefault("aesthetic.tabwidth", 4)
+	viper.SetDefault("aesthetic.useclasses", false)
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			log.Fatal().Err(err).Str("cfgfile", viper.ConfigFileUsed()).Msg("Failure to load config")
+		} else {
+			log.Info().Msg("Creating a new config file!")
+			_, err = os.Create("config.toml")
+			utils.CheckErr(err)
+			viper.WriteConfig()
+		}
+	} else {
+		viper.WatchConfig()
+	}
+
+	viper.WriteConfig()
+
+	ex, err := os.Executable()
+	utils.CheckErr(err)
+
+	binPath := filepath.Dir(ex) + "/"
+
+	viper.Set("docroot", strings.ReplaceAll(viper.GetString("docroot"), "_binary", strings.TrimSuffix(binPath, "/")))
+
+	if viper.GetBool("general.fancystdout") {
+		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).With().Caller().Logger()
+	} else {
+		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+		log.Logger = log.With().Caller().Logger()
+	}
+
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
 	if viper.GetBool("general.debug") {
--- structure/structure.go
+++ structure/structure.go
@@ -1,15 +1,5 @@
 package structure
 
-import (
-	"os"
-	"path/filepath"
-	"strings"
-
-	"gitcat.ca/endigma/holden/utils"
-	"github.com/rs/zerolog/log"
-	"github.com/spf13/viper"
-)
-
 // Page is a struct that holds webpage info for the template
 type Page struct {
 	Prefix           string
@@ -27,52 +17,4 @@ type Directory struct {
 	Directories []Directory
 	Files       []string
 	Name        string
-}
-
-func init() {
-	viper.SetConfigName("config")
-	viper.SetConfigType("toml")
-
-	viper.AddConfigPath("/etc/holden/")
-	viper.AddConfigPath("$HOME/.holden")
-	viper.AddConfigPath(".")
-
-	viper.SetDefault("general.docroot", "docs")
-	viper.SetDefault("general.port", "11011")
-	viper.SetDefault("general.prefix", "")
-	viper.SetDefault("general.workdir", "")
-	viper.SetDefault("general.allowhtml", false)
-	viper.SetDefault("general.linkablelines", true)
-	viper.SetDefault("general.fancycurl", true)
-	viper.SetDefault("general.debug", false)
-	viper.SetDefault("website.sitename", "")
-	viper.SetDefault("website.backtotop", true)
-	viper.SetDefault("website.sidebar", true)
-	viper.SetDefault("aesthetic.highlightstyle", "solarized-dark256")
-	viper.SetDefault("aesthetic.linenumbers", true)
-	viper.SetDefault("aesthetic.linenumbersintable", true)
-	viper.SetDefault("aesthetic.tabwidth", 4)
-	viper.SetDefault("aesthetic.useclasses", false)
-
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-			log.Fatal().Err(err).Str("cfgfile", viper.ConfigFileUsed()).Msg("Failure to load config")
-		} else {
-			log.Info().Msg("Creating a new config file!")
-			_, err = os.Create("config.toml")
-			utils.CheckErr(err)
-			viper.WriteConfig()
-		}
-	} else {
-		viper.WatchConfig()
-	}
-
-	viper.WriteConfig()
-
-	ex, err := os.Executable()
-	utils.CheckErr(err)
-
-	binPath := filepath.Dir(ex) + "/"
-
-	viper.Set("docroot", strings.ReplaceAll(viper.GetString("docroot"), "_binary", strings.TrimSuffix(binPath, "/")))
 }
